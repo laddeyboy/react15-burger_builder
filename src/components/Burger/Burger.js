d2d08457962080a34b52fef640930cb10d5880bd
Josh Ladwig <joshua_ladwig@yahoo.com>
2018-08-31 14:33:14 -0500

    adding basic functionality

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  // before we can 'map' over our ingredients state from BurgerBuilder, we have to convert
+  // ingredients from an object to an array so props.ingredients is {salad:1, bacon:1, cheese:2, meat: 2}
+  // Object.keys(props.ingredients) becomes ["salad", "bacon", "cheese", "meat"]
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      })
+    })
+    .reduce((arr, el) => {
+      return  arr.concat(el)
+    }, [])
+    // console.log("[Burger.js] what is transformedIngredients", transformedIngredients)
+    if(transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );

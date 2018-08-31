c7f2c732ec45018daab2f64ef7df90a654ae97f8
Josh Ladwig <joshua_ladwig@yahoo.com>
2018-08-31 11:12:02 -0500

    complete BurgerIngredient Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
 
+import classes from './Layout.css'
+
 const layout = (props) => (
   <Aux>
     {/* this layout is a basic wrapper for our content that's going to be output to the screen */}
     {/* NOTES: first div is a place holder for our future Navbar */}
     <div>Toolbar, SideDrawer, Backdrop</div>
     {/* NOTES: <main> is going to be our actual content */}
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
